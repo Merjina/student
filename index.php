65000c7789742dd534ab20a62644769efa23d9c9
Merjina <merjina2000@gmail.com>
2024-03-02 00:28:42 -0800

    stu

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+        crossorigin="anonymous"></script>
+</head>
+<body>
+
+  <section style="margin: 50px 0;">
+        <div class="container">
+            <table class="table table-dark">
+                <thead>
+                  <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Studen Name</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Date of Birth</th>
+                    <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                        require_once "conn.php";
+                        $sql_query = "SELECT * FROM students_details";
+                       // print_r($sql_query); exit();
+                        if ($result = $conn->query($sql_query)) {
+                          //  print_r($result); exit();
+                            while ($row = $result -> fetch_assoc()) { 
+                                $Id = $row['id'];
+                                $Name = $row['student_name'];
+                                $Grade = $row['age'];
+                                $Marks = $row['dob'];
+                    ?>
+                    
+                    <tr class="trow">
+                        <td><?php echo $Id; ?></td>
+                        <td><?php echo $Name; ?></td>
+                        <td><?php echo $Grade; ?></td>
+                        <td><?php echo $Marks; ?></td>
+                        <td><a href="updatedata.php?id=<?php echo $Id; ?>" class="btn btn-primary">Edit</a></td>
+                        <td><a href="deletedata.php?id=<?php echo $Id; ?>" class="btn btn-danger">Delete</a></td>
+                    </tr>
+
+                    <?php
+                            } 
+                        } 
+                    ?>
+                </tbody>
+              </table>
+        </div>
+    </section>
+
+</body>
+</html>
+
+
